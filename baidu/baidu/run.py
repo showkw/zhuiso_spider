b33e56b1abdc61104c9686f6da690f0dd74962df
showkw <showkw@163.com>
2018-04-02 01:21:37 +0800

    init

--- baidu/baidu/run.py
+++ baidu/baidu/run.py
@@ -2,5 +2,5 @@
 import time
 from scrapy import cmdline
 
-cmd = "scrapy crawl bdSpider -s LOG_FILE=./logs/Bd-%f.log -s LOG_LEVEL=ERROR" % time.time()
+cmd = "scrapy crawl bdSpider -s LOG_FILE=./baidu/logs/Bd-%f.log -s LOG_LEVEL=ERROR" % time.time()
 cmdline.execute(cmd.split())
